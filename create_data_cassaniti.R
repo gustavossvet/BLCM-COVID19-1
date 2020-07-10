343186d72047b20de631fad581135ef118ac839d
shartn <44002974+shartn@users.noreply.github.com>
2020-07-10 12:52:31 +0200

    Add files via upload

---
+++ create_data_cassaniti.R
@@ -0,0 +1,41 @@
+### data from Cassaniti et al. 2020 ##
+### DOI: 10.1002/jmv.25800
+### 
+
+#  PCR IGG IGM  N
+#   +   +   +   28
+#   +   +   -   2
+#   +   -   +   2
+#   +   -   -   36
+#   -   +   +   0
+#   -   +   -   0
+#   -   -   +   1
+#   -   -   -   41
+28+4+36
+
+setwd("C:\\Users\\admin\\switchdrive\\Cost Harmony\\analysis\\letter")
+
+# create a data set, save it in a matrix
+PCR <- c(rep(1, 68), rep(0, 42))
+length(PCR)
+IGG <- c(rep(1, 30), rep(0, 38), rep(1, 0), rep(0,42))
+length(IGG)
+IGM <- c(rep(1,28),rep(0,2),rep(1,2),rep(0,36),rep(1,0),rep(0,0),rep(1,1),rep(0,41))
+length(IGM)
+
+dat <- data.frame(PCR,IGG,IGM)
+m.ca <- as.matrix(dat)
+
+# dump data for Jags
+dump("m.ca")
+
+# open the dumpdata.R file, add there N and the 110 ones
+N <- 110
+
+ones <- c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+          1, 1, 1, 1, 1, 1, 1, 1)
